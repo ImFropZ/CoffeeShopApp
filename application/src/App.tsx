6bfa5091f0e3e379904842063710e9f847433b34
LimTangmeng <limtangmeng100@gmail.com>
2023-11-02 22:54:38 +0700

    Init: Routes

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,5 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { Dashboard, Menu, Stock, Setting } from "./routes";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/menu" element={<Menu />}></Route>
+      <Route path="/stock" element={<Stock />}></Route>
+      <Route path="/setting" element={<Setting />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ application/src/routes/index.ts
@@ -0,0 +1,6 @@
+import Dashboard from "./Dashboard";
+import Menu from "./Menu";
+import Setting from "./Setting";
+import Stock from "./Stock";
+
+export { Dashboard, Menu, Setting, Stock };
