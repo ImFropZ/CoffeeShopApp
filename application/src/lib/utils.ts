52a6c2fbe12d1b68512220a2b9fbd696f2d84392
LimTangmeng <limtangmeng100@gmail.com>
2023-11-05 17:51:22 +0700

    Added: fullscreen with key 'f11'

--- application/src/lib/utils.ts
+++ application/src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { appWindow } from "@tauri-apps/api/window";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function toggleFullscreen() {
+  const isFullscreen = await appWindow.isFullscreen();
+  if (!isFullscreen) {
+    appWindow.setFullscreen(true);
+  } else {
+    appWindow.setFullscreen(false);
+  }
 }
--- application/src/main.tsx
+++ application/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { HashRouter } from "react-router-dom";
+import { toggleFullscreen } from "./lib/utils";
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "F11") {
+    toggleFullscreen();
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
