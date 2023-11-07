b3a8e6a2e66c4f70c91f94f349de5d77e2ac7e07
FropZ <limtangmeng100@gmail.com>
2023-11-07 18:23:40 +0700

    Added: user edit modal

--- src/components/user/user-columns.tsx
+++ src/components/user/user-columns.tsx
@@ -13,6 +13,27 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "../ui/alert-dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 export type User = {
   id: string;
@@ -46,36 +67,81 @@ export const userColumns: ColumnDef<User>[] = [
     header: "Actions",
     cell: () => {
       return (
-        <AlertDialog>
-          <div className="flex gap-4">
-            <Button
-              variant="outline"
-              className="bg-green-500 text-white hover:bg-green-600 hover:text-white"
-            >
-              <LuPen />
-            </Button>
-            <AlertDialogTrigger asChild>
-              <Button
-                variant="outline"
-                className="bg-red-500 text-white hover:bg-red-600 hover:text-white"
-              >
-                <LuTrash />
-              </Button>
-            </AlertDialogTrigger>
-          </div>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action can not be undone.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+        <Dialog>
+          <AlertDialog>
+            <div className="flex gap-4">
+              <DialogTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="bg-green-500 text-white hover:bg-green-600 hover:text-white"
+                >
+                  <LuPen />
+                </Button>
+              </DialogTrigger>
+              <AlertDialogTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="bg-red-500 text-white hover:bg-red-600 hover:text-white"
+                >
+                  <LuTrash />
+                </Button>
+              </AlertDialogTrigger>
+            </div>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Edit User</DialogTitle>
+                <DialogDescription>
+                  Update your user information.
+                </DialogDescription>
+                <div className="relative">
+                  <div className="flex gap-5">
+                    <div className="flex items-center gap-2">
+                      <Label>Picture</Label>
+                      <Input type="file" />
+                    </div>
+                    <Select>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a permission" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Permissions</SelectLabel>
+                          <SelectItem value="admin">Admin</SelectItem>
+                          <SelectItem value="cashier">Cashier</SelectItem>
+                          <SelectItem value="stockManager">
+                            Stock Manager
+                          </SelectItem>
+                          <SelectItem value="accounting">Accounting</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <Label>Name</Label>
+                  <Input defaultValue={"Lim Tangmeng"} />
+                  <Label>Password</Label>
+                  <Input type="password" />
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="submit">Save changes</Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogHeader>
+            </DialogContent>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action can not be undone.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </Dialog>
       );
     },
   },
