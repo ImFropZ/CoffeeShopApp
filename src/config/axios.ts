3f390b0f3aaa280bfc4cc07a6474b6092a0c600e
Lim Tangmeng <limtangmeng100@gmail.com>
2023-11-30 14:37:45 +0700

    Fixed: login token isnt in the reqeust to backend

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -5,8 +5,18 @@ const instance = axios.create({
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
-    Authorization: "Bearer " + localStorage.getItem("accessToken") || "",
   },
+});
+
+// Add a request interceptor
+instance.interceptors.request.use((config) => {
+  // Do something before request is sent
+  const token = localStorage.getItem("accessToken");
+  config.headers.Authorization =  token ? `Bearer ${token}` : '';
+  return config;
+}, (error) => {
+  // Do something with request error
+  return Promise.reject(error);
 });
 
 export default instance;
